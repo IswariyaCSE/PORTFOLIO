b57684264edb2770abb32ed492bb4eb850a5fde5
IswariyaCSE <107340178+IswariyaCSE@users.noreply.github.com>
2023-07-07 09:22:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+ const menu=document.querySelector(".menu-links");
+ const icon=document.querySelector(".hamburger-icon");
+ menu.classList.toggle("open");
+ icon.classList.toggle("open");
+}
+
+//targetting the menu links and icon and its gonna either open or close
